98c3869a3c5cbe00247709884ae1ec258be44ae5
Omar Kallel <omar.kallel@pivasoftware.com>
2020-06-04 13:03:06 +0100

    remove manufacuter option from cwmp.cpe

--- dmtree/tr181/deviceinfo-iopsyswrt.c
+++ dmtree/tr181/deviceinfo-iopsyswrt.c
@@ -6,11 +6,7 @@
 char * os__get_deviceid_manufacturer()
 {
 	char *v;
-	dmuci_get_option_value_string("cwmp","cpe","manufacturer", &v);
-	if (v[0] == '\0') {
-		db_get_value_string("device", "deviceinfo", "Manufacturer", &v);
-		return v;
-	}
+	db_get_value_string("device", "deviceinfo", "Manufacturer", &v);
 	return v;
 }
 
